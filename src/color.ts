069f9249518d2c0c94e25bf4d3e11dfd3eb83304
JayoonKoo <rnwkdbs12@gmail.com>
2022-01-21 15:28:35 +0900

    feat: add color value

--- src/color.ts
+++ src/color.ts
@@ -1,5 +1,26 @@
 const color = {
-  primary: '#ddd',
+  primary: '#29D3C6',
+  mint01: '#00BAAF',
+  mint03: '#7FEADD',
+  mint04: '#D7FFFA',
+  // blue
+  secondary: '#3CB4F7',
+  blue01: '#3394ED',
+  blue03: '#71D0FF',
+  blue04: '#AEE6FF',
+  // gray
+  titleFont: '#212121',
+  bodyFont01: '#333333',
+  bodyFont02: '#666666',
+  bodyFont03: '#888888',
+  line01: '#A0A0A0',
+  line02: '#C5C5C5',
+  line03: '#E5E5E5',
+  line04: '#ECECEC',
+  bgSurface: '#F8F8F8',
+  bgWhite: '#FFFFFF',
+  // red
+  red: '#FF5555',
 };
 
 export default color;
