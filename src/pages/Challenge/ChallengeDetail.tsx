e2124522b15022bfe62bb4590447b0a8660b42d6
JayoonKoo <rnwkdbs12@gmail.com>
2022-01-24 15:34:22 +0900

    feat: component 추가
    introduceCard, Button

--- src/pages/Challenge/ChallengeDetail.tsx
+++ src/pages/Challenge/ChallengeDetail.tsx
@@ -3,6 +3,7 @@ import {Section} from 'components/Challenge/common';
 import IntroduceCard from 'components/Challenge/common/IntroduceCard';
 import {DetailCard} from 'components/Challenge/DetailCard';
 import {Layout} from 'components/common';
+import Button1 from 'components/common/Button1';
 import Header1 from 'components/common/Header1';
 import styled from 'styled-components';
 
@@ -28,6 +29,38 @@ function ChallengeDetail() {
               imageSrc="https://via.placeholder.com/56x56.jpg"
             />
           </Section>
+          <Section title="리워드 나무">
+            <IntroduceCard
+              description="향나무"
+              imageSrc="https://via.placeholder.com/100x120.jpg"
+            />
+          </Section>
+          <Section title="챌린지 소개">
+            <IntroduceCard
+              isSquare
+              description="1,000km 이동 시 실제 나무 한 그루를 심는 것과 같은 효과를 가지고 있어요."
+              imageSrc="https://via.placeholder.com/200x200.jpg"
+            />
+            <IntroduceCard
+              isMarginTop
+              isSquare
+              description="1,000km 이동 시 실제 나무 한 그루를 심는 것과 같은 효과를 가지고 있어요."
+              imageSrc="https://via.placeholder.com/200x200.jpg"
+            />
+            <IntroduceCard
+              isMarginTop
+              isSquare
+              description="1,000km 이동 시 실제 나무 한 그루를 심는 것과 같은 효과를 가지고 있어요."
+              imageSrc="https://via.placeholder.com/200x200.jpg"
+            />
+            <IntroduceCard
+              isMarginTop
+              isSquare
+              description="1,000km 이동 시 실제 나무 한 그루를 심는 것과 같은 효과를 가지고 있어요."
+              imageSrc="https://via.placeholder.com/200x200.jpg"
+            />
+          </Section>
+          <Button>참여하기</Button>
         </Wrapper>
       </Main>
     </Layout>
@@ -36,10 +69,20 @@ function ChallengeDetail() {
 
 const Main = styled(Layout.Main)`
   background-color: ${color.bgSurface};
+  overflow-y: scroll;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 const Wrapper = styled.div`
   position: absolute;
   left: 0;
+`;
+const Button = styled(Button1)`
+  margin: 40px 24px;
+  border-radius: 10px;
 `;
 
 export {ChallengeDetail};
