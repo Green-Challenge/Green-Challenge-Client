a66fa3ee455b82101972a3ce30677ec33cbfc4ea
JayoonKoo <rnwkdbs12@gmail.com>
2022-01-25 14:40:39 +0900

    feat: Challenging에 ChallengingCard 추가

--- src/pages/Challenge/Challenging.tsx
+++ src/pages/Challenge/Challenging.tsx
@@ -1,5 +1,30 @@
+import {ChallengingCard} from 'components/Challenge/ChallegingCard';
+import {Section} from 'components/Challenge/common';
+import {Layout} from 'components/common';
+import Header from 'components/common/Header';
+
 function Challenging() {
-  return <div>Challenging</div>;
+  return (
+    <Layout>
+      <Layout.Header>
+        <Header isTitle isBig>
+          뚜벅이 출퇴근
+        </Header>
+      </Layout.Header>
+      <Layout.ScrollMain>
+        <ChallengingCard
+          currentDistance="6km"
+          imageSrc="https://via.placeholder.com/312x320.jpg"
+          numberOfPersion={0}
+          progress={60}
+          target="10km"
+        />
+        <Section title="내가 모은 나뭇잎">
+          <div>임심</div>
+        </Section>
+      </Layout.ScrollMain>
+    </Layout>
+  );
 }
 
 export {Challenging};
