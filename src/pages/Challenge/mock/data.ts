004e3806863b622aa7948f719af20bcd5277d435
JayoonKoo <rnwkdbs12@gmail.com>
2022-01-26 20:01:42 +0900

    feat: mock data 추가

---
+++ src/pages/Challenge/mock/data.ts
@@ -0,0 +1,32 @@
+import color from 'color';
+import {Data} from 'components/Challenge/common';
+
+export const data: Data[] = [
+  {
+    id: '이번달',
+    color: color.primary,
+    data: [
+      {x: '1.26', y: 24},
+      {
+        x: '1.27',
+        y: 23,
+      },
+      {
+        x: '1.28',
+        y: 28,
+      },
+      {
+        x: '1.29',
+        y: 21,
+      },
+      {
+        x: '1.30',
+        y: 29,
+      },
+      {
+        x: '1.31',
+        y: 22,
+      },
+    ],
+  },
+];
