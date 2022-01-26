d2895a8e39cd50e4507821beacbab5be09d6a3ed
JayoonKoo <rnwkdbs12@gmail.com>
2022-01-26 15:30:57 +0900

    feat: section 추가

--- src/pages/Challenge/Challenging.tsx
+++ src/pages/Challenge/Challenging.tsx
@@ -1,5 +1,6 @@
 import {ChallengingCard} from 'components/Challenge/ChallegingCard';
-import {Section} from 'components/Challenge/common';
+import CollectionLeafs from 'components/Challenge/CollectionLeafs';
+import {IntroduceCard, Section} from 'components/Challenge/common';
 import {Layout} from 'components/common';
 import Header from 'components/common/Header';
 
@@ -19,8 +20,22 @@ function Challenging() {
           progress={60}
           target="10km"
         />
-        <Section title="내가 모은 나뭇잎">
-          <div>임심</div>
+        <Section title="내가 모은 나뭇잎" info={2}>
+          <CollectionLeafs numberOfLeafs={2} />
+        </Section>
+        <Section title="함께 모은 나뭇잎" info={20}>
+          <IntroduceCard
+            description="나뭇잎 30장이 더 모이면 청소년 향나무로 성장해요"
+            imageSrc="https://via.placeholder.com/200x168.jpg"
+            isBigPadding
+          />
+        </Section>
+        <Section title="나의 탄소 저감량">
+          <IntroduceCard
+            description="나뭇잎 30장이 더 모이면 청소년 향나무로 성장해요"
+            imageSrc="https://via.placeholder.com/200x168.jpg"
+            isBigPadding
+          />
         </Section>
       </Layout.ScrollMain>
     </Layout>
