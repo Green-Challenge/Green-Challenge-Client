8aae8b021ba1095acf2bba19012b2628ddd397bb
JayoonKoo <rnwkdbs12@gmail.com>
2022-01-26 20:02:29 +0900

    feat: Chart 추가

---
+++ src/components/Challenge/common/Chart.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {ResponsiveLine} from '@nivo/line';
+import styled from 'styled-components';
+import color from 'color';
+
+export type Data = {
+  id: string;
+  color: string;
+  data: {x: string; y: number}[];
+};
+
+interface ChartProps {
+  data: Data[];
+}
+
+function Chart({data}: ChartProps) {
+  console.log(data);
+
+  return (
+    <Wrapper>
+      <ResponsiveLine
+        data={data}
+        margin={{top: 50, right: 110, bottom: 50, left: 60}}
+        xScale={{type: 'point'}}
+        yScale={{
+          type: 'linear',
+          min: 'auto',
+          max: 'auto',
+          stacked: true,
+          reverse: false,
+        }}
+        yFormat=" >-.2f"
+        pointSize={10}
+        pointColor={{theme: 'background'}}
+        pointBorderWidth={2}
+        pointBorderColor={{from: 'serieColor'}}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  background-color: ${color.bgWhite};
+  width: 312px;
+  height: 352px;
+  text-align: center;
+  padding-top: 30px;
+  border-radius: 10px;
+`;
+
+export {Chart};
