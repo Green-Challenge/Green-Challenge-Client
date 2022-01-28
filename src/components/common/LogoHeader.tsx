206ec30ff26e2777436e35ef31d83db18f695c04
JayoonKoo <rnwkdbs12@gmail.com>
2022-01-28 16:11:38 +0900

    feat: LogoHeader 추가

--- src/components/common/LogoHeader.tsx
+++ src/components/common/LogoHeader.tsx
@@ -1,5 +1,22 @@
-function LogoHeader() {
-  return <div></div>;
+import {css} from 'styled-components/macro';
+import Header, {wrapperStyle} from './Header';
+
+interface LogoHeaderProps {
+  logo: React.ReactNode;
+  headerRightItems?: React.ReactNode;
 }
+function LogoHeader({headerRightItems, logo}: LogoHeaderProps) {
+  return (
+    <div css={wrapperStyle}>
+      <div css={leftStyle}>{logo}</div>
+      <Header.Right>{headerRightItems}</Header.Right>
+    </div>
+  );
+}
+
+const leftStyle = css`
+  width: 90%;
+  display: flex;
+`;
 
 export default LogoHeader;
