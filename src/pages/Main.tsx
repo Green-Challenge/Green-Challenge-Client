d725f04b36afc10f8cca9c7c9007a2ed8222e90c
JayoonKoo <rnwkdbs12@gmail.com>
2022-01-28 16:11:21 +0900

    feat: Layout만 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,23 @@
+import {Layout} from 'components/common';
+import LogoHeader from 'components/common/LogoHeader';
+import Icon from 'components/Icon/Icon';
+import {css} from 'styled-components/macro';
+
 function Main() {
-  return <div>Main</div>;
+  return (
+    <Layout>
+      <Layout.Header>
+        <LogoHeader
+          logo={<Icon name="logo" css={IconStyle} />}
+          headerRightItems={<Icon name="my" css={IconStyle} />}
+        />
+      </Layout.Header>
+    </Layout>
+  );
 }
+
+const IconStyle = css`
+  cursor: pointer;
+`;
 
 export default Main;
--- src/pages/My/MyTree.tsx
+++ src/pages/My/MyTree.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import {Layout} from 'components/common';
+import Header from 'components/common/Header';
+import Icon from 'components/Icon/Icon';
+import {css} from 'styled-components/macro';
 
 function MyTree() {
-  return <div>MyTree</div>;
+  return (
+    <Layout>
+      <Layout.Header>
+        <Header headerRgihtItems={<Icon name="close" css={IconStyle} />}>
+          함께 심은 나무
+        </Header>
+      </Layout.Header>
+    </Layout>
+  );
 }
+
+const IconStyle = css`
+  width: 24px;
+  cursor: pointer;
+`;
 
 export {MyTree};
