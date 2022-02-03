73eb7976eb80821072b80b5f17d4a4727a49384a
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-02-03 09:11:33 +0900

    Docs: map함수 일부 수정

--- src/components/Main/ChallengeSection/ChallengeSection.tsx
+++ src/components/Main/ChallengeSection/ChallengeSection.tsx
@@ -3,20 +3,23 @@ import ChallengeCard from './ChallengeCard';
 import Challenges from './Challenges';
 
 function ChallengeSection() {
-  const renderChallenges = Challenges.map(challenge => {
-    return (
-      <ChallengeCard
-        percentage={challenge.percentage}
-        challengeName={challenge.ChallengeName}
-        rewardToken={challenge.rewardToken}
-        numberOfChallengers={challenge.numberOfChallengers}
-        isComplete={challenge.isComplete}
-        isParticipating={challenge.isParticipating}
-        treeId={challenge.treeId}
-        key={challenge.treeId}
-      />
-    );
-  });
-  return <Section title="챌린지 참여">{renderChallenges}</Section>;
+  return (
+    <Section title="챌린지 참여">
+      {Challenges.map(challenge => {
+        return (
+          <ChallengeCard
+            percentage={challenge.percentage}
+            challengeName={challenge.ChallengeName}
+            rewardToken={challenge.rewardToken}
+            numberOfChallengers={challenge.numberOfChallengers}
+            isComplete={challenge.isComplete}
+            isParticipating={challenge.isParticipating}
+            treeId={challenge.treeId}
+            key={challenge.treeId}
+          />
+        );
+      })}
+    </Section>
+  );
 }
 export {ChallengeSection};
