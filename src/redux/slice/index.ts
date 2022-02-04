bad1734f0da461d04ed396a3ea5b18e32f7f86f7
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-04 20:44:38 +0900

    feat: challenge redux 추가

--- src/redux/slice/index.ts
+++ src/redux/slice/index.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import auth from './auth/auth';
+import challenges from './challenges/challenges';
 
 const rootReducer = combineReducers({
   auth,
+  challenges,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
