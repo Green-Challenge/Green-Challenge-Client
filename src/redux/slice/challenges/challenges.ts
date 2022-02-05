da80b501b10b608e51589872329ce6c8dd4dd4d8
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-05 12:31:21 +0900

    refactor: reduxutils 리팩토링
    flag 인수를 받지 않도록
    함수에 인수를 줄이도록
    -> FecthThunkData 클래스 생성

--- src/redux/slice/challenges/challenges.ts
+++ src/redux/slice/challenges/challenges.ts
@@ -1,7 +1,7 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import {ChallengeService} from 'service/challenges/challenges';
 import {ChallengesRes} from 'service/challenges/type';
-import {fetchThunk, getInitialState} from 'utils/reduxUtils';
+import {FetchThunkData, getInitialState} from 'utils/reduxUtils';
 
 export const fetchChallenges = createAsyncThunk(
   'challenges/fetchChallenges',
@@ -12,15 +12,17 @@ const initialState = {
   challenges: getInitialState<ChallengesRes>(),
 };
 
+const fetchThunkKeepDataNoHandler = new FetchThunkData({
+  fetch: fetchChallenges,
+  keep: true,
+  key: 'challenges',
+});
+
 const challengesSlice = createSlice({
   name: 'challenges',
   initialState,
   reducers: {},
-  extraReducers: fetchThunk<ChallengesRes, typeof initialState>(
-    fetchChallenges,
-    'challenges',
-    true,
-  ),
+  extraReducers: fetchThunkKeepDataNoHandler.getFetchThunkReducer(),
 });
 
 export default challengesSlice.reducer;
