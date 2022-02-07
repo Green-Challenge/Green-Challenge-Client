f74f61508da0110884719a7283ac387609336937
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-08 02:22:49 +0900

    feat: challengeDetail thunk 추가

--- src/redux/slice/challenges/challenges.ts
+++ src/redux/slice/challenges/challenges.ts
@@ -1,28 +1,49 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import {ChallengeService} from 'service/challenges/challenges';
-import {ChallengesRes} from 'service/challenges/type';
-import {FetchThunkData, getInitialState} from 'utils/reduxUtils';
+import {ChallengeDetailRes, ChallengesRes} from 'service/challenges/type';
+import {
+  FetchThunkData,
+  getInitialState,
+  InitialStateById,
+} from 'utils/reduxUtils';
 
 export const fetchChallenges = createAsyncThunk(
   'challenges/fetchChallenges',
   ChallengeService.getChallenges,
 );
 
+export const fetchChallengeDetail = createAsyncThunk(
+  'challenges/fetchChallengeDetail',
+  ChallengeService.getChallengeDetail,
+);
+
+const challengeDetail: InitialStateById<ChallengeDetailRes> = {};
+
 const initialState = {
   challenges: getInitialState<ChallengesRes>(),
+  challengeDetail,
 };
 
-const fetchThunkKeepDataNoHandler = new FetchThunkData({
+const fetchChallengeKeepDataNoHandler = new FetchThunkData({
   fetch: fetchChallenges,
   keep: true,
   key: 'challenges',
+});
+
+const fetchChallengeDetailKeepDataWithHandler = new FetchThunkData({
+  fetch: fetchChallengeDetail,
+  keep: true,
+  key: 'challengeDetail',
 });
 
 const challengesSlice = createSlice({
   name: 'challenges',
   initialState,
   reducers: {},
-  extraReducers: fetchThunkKeepDataNoHandler.getFetchThunkReducer(),
+  extraReducers: builder => {
+    fetchChallengeKeepDataNoHandler.getFetchThunkReducer(builder);
+    fetchChallengeDetailKeepDataWithHandler.getFetchThunkReducerById(builder);
+  },
 });
 
 export default challengesSlice.reducer;
