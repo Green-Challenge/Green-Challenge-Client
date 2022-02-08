222a730d43f71d161618ed60ba9a45791fa81d8c
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-08 17:00:29 +0900

    feat: signin slice 추가

--- src/redux/slice/auth/auth.ts
+++ src/redux/slice/auth/auth.ts
@@ -5,6 +5,7 @@ import {User} from './type';
 
 export const signUp = createAsyncThunk('auth/signUp', AuthService.signUp);
 export const me = createAsyncThunk('auth/me', AuthService.me);
+export const signIn = createAsyncThunk('auth/signIn', AuthService.signIn);
 // export const signIn = createAsyncThunk()
 
 const initialState = {
@@ -18,12 +19,19 @@ const fetchSignUp = new FetchThunkData({
   key: 'register',
 });
 
+const fetchSignIn = new FetchThunkData({
+  fetch: signIn,
+  keep: false,
+  key: 'user',
+});
+
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
   extraReducers: builder => {
     fetchSignUp.getFetchThunkReducer(builder);
+    fetchSignIn.getFetchThunkReducer(builder);
   },
 });
 
