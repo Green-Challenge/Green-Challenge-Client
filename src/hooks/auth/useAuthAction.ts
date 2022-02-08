21ce02933ce134b06209d9874b2d65143b1612dc
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-08 17:00:06 +0900

    feat: signin thunk creator 추가

--- src/hooks/auth/useAuthAction.ts
+++ src/hooks/auth/useAuthAction.ts
@@ -1,9 +1,12 @@
 import {useMemo} from 'react';
 import {bindActionCreators} from 'redux';
 import {useAppDispatch} from '../storeHooks';
-import {signUp} from 'redux/slice/auth/auth';
+import {signUp, signIn} from 'redux/slice/auth/auth';
 
 export default function useAuthActions() {
   const dispatch = useAppDispatch();
-  return useMemo(() => bindActionCreators({signUp}, dispatch), [dispatch]);
+  return useMemo(
+    () => bindActionCreators({signUp, signIn}, dispatch),
+    [dispatch],
+  );
 }
