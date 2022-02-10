fa848022e95c02992c2bad9a75db0964e4aed313
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-02-10 16:15:41 +0900

    Docs: isParticipating에 따른 프로그래스바 value값 변경

--- src/components/Main/ChallengeSection/ChallengeCircle.tsx
+++ src/components/Main/ChallengeSection/ChallengeCircle.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {css} from 'styled-components/macro';
 import {CircularProgressbarWithChildren} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import {css} from 'styled-components/macro';
 import Icon from 'components/Icon/Icon';
 import styled from 'styled-components';
 
@@ -26,77 +26,39 @@ function ChallengeCircle({
   const treeName = treeNameById(treeId);
   return (
     <Wrapper>
-      {isParticipating ? (
-        <div style={{width: 86, height: 86}}>
-          <CircularProgressbarWithChildren
-            counterClockwise
-            value={percentage}
-            text={`${treeName}`}
-            background={true}
-            styles={{
-              root: {},
-              path: {
-                stroke: '#7FEADD',
-                strokeLinecap: 'round',
-                transition: 'stroke-dashoffset 0.5s ease 0s',
-                transform: 'rotate(1turn)',
-                transformOrigin: 'center center',
-              },
-              trail: {
-                stroke: '#F2F2F2',
-                strokeLinecap: 'round',
-                transform: 'rotate(0.5turn)',
-                transformOrigin: 'center center',
-              },
-              text: {
-                // fill: 'transparent',
-                fill: '#000',
-                fontSize: '1rem',
-              },
-              background: {
-                fill: '#F2F2F2',
-              },
-            }}>
-            {isComplete ? <Icon name="success" css={Success} /> : null}
-            <Img src={`/Icon/${treeName}.svg`} alt="tree" />
-          </CircularProgressbarWithChildren>
-        </div>
-      ) : (
-        <div style={{width: 86, height: 86}}>
-          <CircularProgressbarWithChildren
-            counterClockwise
-            value={percentage}
-            text={`${treeName}`}
-            background={true}
-            styles={{
-              root: {},
-              path: {
-                stroke: 'transparent',
-                strokeLinecap: 'round',
-                transition: 'stroke-dashoffset 0.5s ease 0s',
-                transform: 'rotate(1turn)',
-                transformOrigin: 'center center',
-              },
-              trail: {
-                stroke: '#F2F2F2',
-                strokeLinecap: 'round',
-                transform: 'rotate(0.5turn)',
-                transformOrigin: 'center center',
-              },
-              text: {
-                // fill: 'transparent',
-                fill: '#000',
-                fontSize: '1rem',
-              },
-              background: {
-                fill: '#F2F2F2',
-              },
-            }}>
-            {isComplete ? <Icon name="success" css={Success} /> : null}
-            <Img src={`/Icon/${treeName}.svg`} alt="tree" />
-          </CircularProgressbarWithChildren>
-        </div>
-      )}
+      <div style={{width: 86, height: 86}}>
+        <CircularProgressbarWithChildren
+          counterClockwise
+          value={isParticipating ? percentage : 0}
+          text={`${treeName}`}
+          background={true}
+          styles={{
+            root: {},
+            path: {
+              stroke: '#7FEADD',
+              strokeLinecap: 'round',
+              transition: 'stroke-dashoffset 0.5s ease 0s',
+              transform: 'rotate(1turn)',
+              transformOrigin: 'center center',
+            },
+            trail: {
+              stroke: '#F2F2F2',
+              strokeLinecap: 'round',
+              transform: 'rotate(0.5turn)',
+              transformOrigin: 'center center',
+            },
+            text: {
+              fill: 'transparent',
+              fontSize: '1rem',
+            },
+            background: {
+              fill: '#F2F2F2',
+            },
+          }}>
+          {isComplete ? <Icon name="success" css={Success} /> : null}
+          <Img src={`/Icon/${treeName}.svg`} alt="tree" />
+        </CircularProgressbarWithChildren>
+      </div>
     </Wrapper>
   );
 }
@@ -116,5 +78,9 @@ const Img = styled.img`
   width: 5.375rem;
   height: 5.375rem;
 `;
+
+// const ColorStyle = css`
+//   color: 'transparent';
+// `;
 
 export default ChallengeCircle;
