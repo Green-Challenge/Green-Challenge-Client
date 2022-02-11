f42620bec7db950ef34ab9120479b80ebfb8f750
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-02-11 23:36:58 +0900

    Feat: add 토큰 박스

--- src/components/MyPage/MyProfileSection.tsx
+++ src/components/MyPage/MyProfileSection.tsx
@@ -14,12 +14,18 @@ function MyProfileSection() {
         <p css={AddressInfo}>서울시 강남구</p>
         <p css={NickInfo}>닉네임</p>
       </InfoBox>
+      <TokenBox>
+        <Token>
+          <Icon name="token" css={TokenStyle} />
+          <span>3,900</span>
+        </Token>
+      </TokenBox>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.section`
-  padding-top: 20px;
+  padding-top: 1.25rem;
   width: 100%;
   box-sizing: border-box;
   background-color: ${color.bgWhite};
@@ -47,18 +53,45 @@ const IconStyle = css`
 
 const InfoBox = styled.div`
   margin-top: 0.75rem;
+  margin-bottom: 1.5rem;
   text-align: center;
 `;
 
 const AddressInfo = css`
   color: ${color.bodyFont03};
-  font-size: 14px;
+  font-size: 0.875rem;
 `;
 
 const NickInfo = css`
   color: ${color.bodyFont01};
-  font-size: 20px;
+  font-size: 1.25rem;
   font-weight: bold;
+`;
+
+const TokenBox = styled.div`
+  width: 5.5rem;
+  height: 2.25rem;
+  background-color: ${color.line04};
+  border-radius: 0.75rem;
+  margin: 0 auto;
+  box-sizing: border-box;
+  padding: 0.375rem 0.938rem;
+`;
+
+const Token = styled.div`
+  text-align: center;
+  display: flex;
+  color: ${color.bodyFont01};
+  span {
+    font-size: 0.75rem;
+  }
+  margin-top: 0.125rem;
+`;
+
+const TokenStyle = css`
+  width: 1.172rem;
+  height: 1.172rem;
+  margin-right: 0.375rem;
 `;
 
 // const Img = styled.img`
