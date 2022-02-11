2b1d47c6fc256373a0ebe3e1dd748582e4f5e974
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-12 08:50:42 +0900

    feat: record

--- src/pages/Challenge/Record.tsx
+++ src/pages/Challenge/Record.tsx
@@ -3,47 +3,65 @@ import {Layout} from 'components/common';
 import Button from 'components/common/Button';
 import Header from 'components/common/Header';
 import {useIsAuthPush} from 'hooks/auth/useIsAuth';
-import {useLocation} from 'react-router-dom';
+import useRecord from 'hooks/challenge/useRecord';
+import {useHistory, useLocation} from 'react-router-dom';
 import styled from 'styled-components';
 import {css} from 'styled-components/macro';
 
 //#오늘의 기록
 function Record() {
   useIsAuthPush();
+  const history = useHistory();
   const {
     state: {userId, challengeId},
   } = useLocation<{userId: number; challengeId: number}>();
 
-  console.log(userId, challengeId);
+  const onClick = () => {
+    history.push({pathname: '/'});
+  };
+
+  const {data, loading, error} = useRecord(userId, challengeId);
+
+  if (loading) {
+    return <div>로딩중</div>;
+  }
+
+  if (error) {
+    return <div>에러</div>;
+  }
 
   return (
-    <Layout>
-      <Layout.Header>
-        <Header isTitle isBig>
-          오늘의 기록
-        </Header>
-      </Layout.Header>
-      <Main>
-        <Wrapper>
-          <div css={BoxStyle}>
-            <Distance>
-              <div css={Info}>이동 거리</div>
-              <div css={InfoNumber}>00m</div>
-            </Distance>
-            <Carbon>
-              <div css={Info}>탄소저감량</div>
-              <div css={InfoNumber}>00kg</div>
-            </Carbon>
-          </div>
-          <img
-            src="/Icon/SVG/earth_Today.svg"
-            alt="지구캐릭터"
-            css={EarthStyle}
-          />
-        </Wrapper>
-        <Button>확인</Button>
-      </Main>
-    </Layout>
+    <>
+      {data && (
+        <Layout>
+          <Layout.Header>
+            <Header isTitle isBig>
+              오늘의 기록
+            </Header>
+          </Layout.Header>
+          <Main>
+            <Wrapper>
+              <div css={BoxStyle}>
+                <Distance>
+                  <div css={Info}>이동 거리</div>
+                  <div css={InfoNumber}>{parseInt(data.distance) * 1000}m</div>
+                </Distance>
+                <Carbon>
+                  <div css={Info}>탄소저감량</div>
+                  <div css={InfoNumber}>{data.reducedCarbon.toFixed()}kg</div>
+                </Carbon>
+              </div>
+              <img
+                src="/Icon/SVG/earth_Today.svg"
+                alt="지구캐릭터"
+                css={EarthStyle}
+              />
+            </Wrapper>
+            <Button onClick={onClick}>확인</Button>
+          </Main>
+        </Layout>
+      )}
+    </>
   );
 }
 
