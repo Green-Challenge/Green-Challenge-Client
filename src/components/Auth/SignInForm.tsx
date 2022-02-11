94f89fd6aacb15396fe6fafb3aa634c7ec01f646
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-02-11 13:15:52 +0900

    Docs: px to rem 수정

--- src/components/Auth/SignInForm.tsx
+++ src/components/Auth/SignInForm.tsx
@@ -98,8 +98,6 @@ const SignUp = styled.span`
 const Btn = styled(Button)`
   background-color: ${props => (props.color ? color.line03 : color.primary)};
   color: ${props => props.color || color.bgWhite};
-  /* position: fixed;
-  bottom: 0; */
   margin-top: 50px;
 `;
 
